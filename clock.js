a3f438906e155fa7b3ecc87c1c73bcc496285333
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-12 21:59:11 +0900

    #3.0 Making a JS Clock part One
    seconds 추가

--- clock.js
+++ clock.js
@@ -5,7 +5,8 @@ function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  clockTitle.innerText = `${hours}:${minutes}`;
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function init() {
