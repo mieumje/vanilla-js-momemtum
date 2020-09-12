de4e28b5dd88cf4ebe1bc330dde85a1ad7275ae9
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-13 00:00:40 +0900

    #3.3 Saving the User Name part Two
    local storage에 이름 저장하는 방법

--- greeting.js
+++ greeting.js
@@ -1,9 +1,25 @@
 const form = document.querySelector(".js-form"),
-  input = form.querySelector(".input"),
+  input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
@@ -15,6 +31,7 @@ function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     //who is not
+    askForName();
   } else {
     //who is
     paintGreeting(currentUser);
