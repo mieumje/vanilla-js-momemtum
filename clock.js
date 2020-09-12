7c5a21bde2240ce1a92bc0fa4712de6c4d6a9567
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-12 21:58:29 +0900

    #3.0 Making a JS Clock part One
    시간을 가져오는 함수 선언
    title을 가져온 시간으로 초기화

--- clock.js
+++ clock.js
@@ -1,5 +1,15 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
-function init() {}
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  clockTitle.innerText = `${hours}:${minutes}`;
+}
+
+function init() {
+  getTime();
+}
 
 init();
