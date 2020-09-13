040e3c5846c7f6e3ac26a9322ab0c7f7c5ac7d05
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-13 18:49:47 +0900

    #3.5 Making a To Do List part Two
    to do list에 들어갈 array를 생성

--- todo.js
+++ todo.js
@@ -4,6 +4,8 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
@@ -13,6 +15,11 @@ function paintToDo(text) {
   li.appendChild(span);
   li.appendChild(delBtn);
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: toDos.length + 1,
+  };
+  toDos.push(toDoObj);
 }
 
 function handleSubmit(event) {
