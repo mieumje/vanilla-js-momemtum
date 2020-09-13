69457ff262e2fd94c6dea447fd151865957a2d63
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-13 19:52:43 +0900

    #3.7 Image Background
    img 삽입을 위해
    paintImage 함수 선언
    init에서 paintImage 함수 호출

--- background.js
+++ background.js
@@ -2,13 +2,18 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
+function paintImage(imgNumber) {
+  const image = new Image();
+}
+
 function genRandom() {
-  const number = Math.floor(Math.random() * IMG_NUMBE);
+  const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
 
 function init() {
   const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
