2d1ef47339a9c0849a6360c03da8515856eb31c8
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-13 18:46:45 +0900

    #3.4 Making a To Do List part One
    to do list를 만들고
    submit을 하면 li에 value가 생기도록 함
    inputvalue, X emoji

--- todo.js
+++ todo.js
@@ -1,17 +1,36 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = form.document.querySelector("input"),
+  toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
+const TODOS_LS = "toDos";
 
-const TODOS_LS = 'toDos';
-
-function loadToDos(){
-    const = toDos = localStorage.getItem(TODOS_LS);
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
 
-function init(){
-    loadToDos();
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
