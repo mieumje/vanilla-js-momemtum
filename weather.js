1f497fa52d5110f75eecaa026bbbf18c353e214c
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-14 20:47:38 +0900

    #3.8 Getting the Weather part One Geolocation
    loadCoords 함수 선언

--- weather.js
+++ weather.js
@@ -1,3 +1,16 @@
-function init() {}
+const COORDS = "coords";
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadCoords === null) {
+    askForCoords();
+  } else {
+    //get weather
+  }
+}
+
+function init() {
+  loadCoords();
+}
 
 init();
