c91d8a1b3533dc2b97ad4c98573b7ffeccddb3c5
mieumje <66072832+mieumje@users.noreply.github.com>
2020-09-14 20:49:24 +0900

    #3.8 Getting the Weather part One Geolocation
    askForCoords 함수선언

--- weather.js
+++ weather.js
@@ -1,5 +1,9 @@
 const COORDS = "coords";
 
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition();
+}
+
 function loadCoords() {
   const loadedCords = localStorage.getItem(COORDS);
   if (loadCoords === null) {
